8423524ca717f6962697fa2e6ee0311dbf0e74f9
unknown <dqp200815@163.com>
2017-02-15 13:53:30 +0800

    Signed-off-by: unknown <dqp200815@163.com>

---
+++ CurrentYear_Cit_Crawl.py
@@ -0,0 +1,167 @@
+#coding:utf-8
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from nt import chdir
+import time
+import os
+import threading 
+
+def paper_info(filename):
+	global papers,unlock_uts
+	rows = [i.replace('\n','').split('\t') for i in open('2012_papers.txt','r').readlines()[1:]]
+	papers = [[j[60],j[44]] for j in rows]
+	print 'paper number of filename is %s' %len(papers)
+	unlock_uts = [i for i in range(len(papers))]
+
+def click(browser,xpath,sleeptime=2):
+	response = False
+	Wait_Time = 0
+	while Wait_Time<=30:
+		try:
+			element = browser.find_element_by_xpath(xpath)
+			browser.execute_script("arguments[0].scrollIntoView();", element) #拖动滚动条到元素可见的地方
+			element.click()
+			response = True
+			time.sleep(2)
+			break
+		except Exception,ex:
+			time.sleep(2)
+			Wait_Time+=2
+			print '链接%s获取失败,等待2s后重新解析....' %xpath
+	if Wait_Time>30:
+		print '等待超时,重新执行此记录....'
+	return response
+
+def extr_cit(cit_str,py):
+	num = '0'
+	per_cit = cit_str.encode('utf-8').strip().split('\n')
+	for cit in per_cit:
+		if py in cit:
+			l = cit.find('(')
+			r = cit.find(')')
+			num = cit[l+1:r]
+			break
+	return num
+class CitCrawlThread():
+
+def cit_current():
+	browser = webdriver.Chrome()
+	host = 'http://apps.webofknowledge.com/' #打开主页而
+	browser.get(host)
+	xpath = "//a[@id='databases']" 
+	if click(browser,xpath):
+		xpath = "//i[@id='collectionDropdown']/ul/li[2]/a" #选择核心数据库
+		if click(browser,xpath):
+			xpath = "//div[@class='select2-container j-custom-select select-criteria2']/a"
+			if click(browser,xpath):
+				xpath = "//select[@class='j-custom-select select-criteria2 select2-offscreen']/option[17]" #选择按入藏号检索
+				if click(browser,xpath):
+					flag = 1
+					while len(unlock_uts)>0:
+						status = False
+						paper_index = unlock_uts[0]
+						ut = papers[paper_index][0]
+						py = papers[paper_index][1]
+						unlock_uts.remove(paper_index)
+						num = '0'
+						for i in range(5):
+						#打开基础检索界面以入藏号进行检索，第一次检索和返回继续检索界面存在一些不同，需要判断。
+							if flag==1:#第一次检索
+								txt_input = browser.find_element_by_xpath("//input[@class='search-criteria-input ui-autocomplete-input']")
+								txt_input.send_keys(ut)
+							if flag>1:#返回继续检索
+								'''xpath = "//i[@id='clearIcon1']"
+								if click(browser,xpath):
+									txt_input = browser.find_element_by_xpath("//div[@class='search-criteria-input-wr']/input[@class='search-criteria-input']")
+									txt_input.send_keys(ut)'''
+								txt_input = browser.find_element_by_xpath("//div[@class='search-criteria-input-wr']/input[@class='search-criteria-input']")
+								txt_input.clear()
+								txt_input.send_keys(ut)
+
+							xpath = "//span[@class='searchButton']/input"#提交并检索
+							if click(browser,xpath):
+								flag+=1
+								xpath = "//div[@class='search-results-data-cite']/a" #打开施引文献列表
+								cit_num = browser.find_element_by_xpath(xpath).text.encode('utf8')
+								if not cit_num =='0':
+									if click(browser,xpath):
+										if cit_type=='esi': #获取ESI被引频次
+											xpath = "//div[@class='block-text-content-scorecard']/p[@class='viewScorecard']/a[@class='scoreCardOn']"
+											click(browser,xpath)
+											xpath = "//table[@class='All_Cit_Level3']/tbody/tr[1]/td[2]/span/a"
+											if click(browser,xpath):
+												response,num = analy_citpage(browser,py)
+										
+										if cit_type=='wos':#获取WOS被引频次
+											xpath = "//div[@id='PublicationYear']/h4/a" #打开出版年筛选条件
+											if click(browser,xpath):
+												py_list = browser.find_element_by_xpath("//div[@id='PublicationYear']/div[@class='refine-content']/div[@class='refine-subitem-list']").text
+												num = extr_cit(py_list,py)
+												if num=='0':
+													xpath = "//a[@class='link-style1'][@id='PublicationYear']" #查看出版年更多选项
+													try:
+														browser.find_element_by_xpath(xpath)
+														if click(browser,xpath):
+															py_list = browser.find_element_by_xpath("//tr[@id='PublicationYear_raMore_tr']").text
+															num = extr_cit(py_list,py)
+														else:
+															print '第%s次重新执行此记录：%s' %(str(i+1),org_name)
+															xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" #返回检索界面
+															click(browser,xpath)
+															continue
+													except	Exception,ex:
+														num=='0'
+											else:
+												print '第%s次重新执行此记录：%s' %(str(i+1),org_name)
+												xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" #返回检索界面
+												click(browser,xpath)
+												continue
+									else:
+										print '第%s次重新执行此记录：%s' %(str(i+1),org_name)
+										xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" #返回检索界面
+										click(browser,xpath)
+										continue
+									cit_str = '\t'.join([ut,py,num])
+									f = open('cur_year_cit.txt','a')
+									f.write(cit_str+'\n')
+									f.close()
+									print '---'+str(paper_index+1)+'---'
+									status = True
+									xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" #返回检索界面
+									click(browser,xpath)
+									break
+							else:
+								print '第%s次重新执行此记录：%s' %(str(i+1),org_name)
+								xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" #返回检索界面
+								click(browser,xpath)
+								continue
+
+						if not status:
+							print '记录：%s 引用分析失败！' %ut
+							unlock_uts.append(paper_index)
+					browser.quit()
+
+def my_thread(threads_num):
+	threads = [] 
+	for i in range(0,threads_num):
+		threads_name = 'thread_'+str(i+1)
+		t = threading.Thread(target=cit_current)
+		threads.append(t)
+   	for i in range(0,len(threads)):
+   		threads[i].start() 
+   		print 'start thread '+str(i+1)
+   		time.sleep(10)
+   	for thread in threads:
+   		thread.join()
+
+
+if __name__=='__main__':
+	global cit_type,filename
+	filename = '2012_papers.txt'
+	cit_type = 'wos'	
+	paper_info(filename)
+	f = open('cur_year_cit.txt','a')
+	f.write('UT\tPY\t'+cit_type+'_Cit\n')
+	f.close()
+	my_thread(3)
---
+++ CurrentYear_Cit_Crawl_4 .py
@@ -0,0 +1,152 @@
+#coding:utf-8
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+from selenium.webdriver.support.wait import WebDriverWait
+from nt import chdir
+import time
+import os
+import threading
+
+def paper_info(filename):
+	global papers,unlock_uts
+	rows = [i.replace('\n','').split('\t') for i in open(filename,'r').readlines()[1:]]
+	papers = [[j[60],j[44]] for j in rows]
+	print 'paper number of filename is %s' %len(papers)
+	unlock_uts = [i for i in range(len(papers))]
+
+def click(browser,xpath,sleeptime=2):
+	response = None
+	try:
+		wait = WebDriverWait(browser,120)
+		wait.until(lambda x: x.find_element_by_xpath(xpath))	
+		element = browser.find_element_by_xpath(xpath)
+		browser.execute_script("arguments[0].scrollIntoView();", element) #拖动滚动条到元素可见的地方
+		element.click()
+		time.sleep(2)
+		response = True
+	except Exception,ex:
+		response = False
+	return response
+
+def find_element(browser,xpath):
+	response = None
+	try:
+		browser.find_element_by_xpath(xpath)
+		response = True
+	except Exception,ex:
+		response = False
+	return response
+
+def extr_cit(cit_str,py):
+	num = '0'
+	per_cit = cit_str.encode('utf-8').strip().split('\n')
+	for cit in per_cit:
+		if py in cit:
+			l = cit.find('(')
+			r = cit.find(')')
+			num = cit[l+1:r]
+			break
+	return num
+
+def cit_current():
+	drive_state = None
+	for i in range(3):
+		try:
+			browser = webdriver.Chrome()
+			host = 'http://apps.webofknowledge.com/' #打开主页而
+			browser.get(host)
+			xpath = "//a[@id='databases']" 
+			if click(browser,xpath):
+				xpath = "//i[@id='collectionDropdown']/ul/li[2]/a" #选择核心数据库
+				if click(browser,xpath):
+					xpath = "//div[@class='select2-container j-custom-select select-criteria2']/a"
+					if click(browser,xpath):
+						xpath = "//select[@class='j-custom-select select-criteria2 select2-offscreen']/option[17]" #选择按入藏号检索
+						click(browser,xpath)
+						drive_state = True
+						break
+		except Exception,ex:
+			print '浏览器启动超时，retry....'
+	if drive_state is None:
+		print '浏览器启动失败，线程退出！'
+		exit()
+	flag = 1
+	while len(unlock_uts)>0:
+		try:
+			paper_index = unlock_uts[0]
+			ut = papers[paper_index][0]
+			py = papers[paper_index][1]
+			unlock_uts.remove(paper_index)
+			num = None
+
+			#获取文本框输入UT号
+			if flag==1:#第一次检索
+				txt_input = browser.find_element_by_xpath("//input[@class='search-criteria-input ui-autocomplete-input']")
+				txt_input.send_keys(ut)
+			if flag>1:#返回继续检索
+				txt_input = browser.find_element_by_xpath("//div[@class='search-criteria-input-wr']/input[@class='search-criteria-input']")
+				txt_input.clear()
+				txt_input.send_keys(ut)
+			#提交并检索
+			xpath = "//span[@class='searchButton']/input"
+			if click(browser,xpath):
+				#获取引用文献列表
+				xpath = "//div[@class='search-results-data-cite']/a"
+				if find_element(browser,xpath):
+					if click(browser,xpath):
+						#打开出版年筛选条件
+						xpath = "//div[@id='PublicationYear']/h4/a" 
+						if click(browser,xpath):
+							py_list = browser.find_element_by_xpath("//div[@id='PublicationYear']/div[@class='refine-content']/div[@class='refine-subitem-list']").text
+							num = extr_cit(py_list,py)
+							if num=='0':
+								#查看出版年更多选项
+								xpath = "//a[@class='link-style1'][@id='PublicationYear']" 
+								if find_element(browser,xpath):
+									if click(browser,xpath):
+										py_list = browser.find_element_by_xpath("//tr[@id='PublicationYear_raMore_tr']").text
+										num = extr_cit(py_list,py)
+								else:
+									num=='0'
+				else:
+					num = '0'
+			cit_str = '\t'.join([ut,py,num])
+			f = file('cur_year_cit.txt','a').write(cit_str+'\n')
+			print '---%s---' %str(paper_index+1)
+			#返回检索界面继续检索
+			xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" 
+			click(browser,xpath)
+			flag+=1
+
+		except Exception,ex:
+			print '记录：%s 引用分析失败！' %ut
+			f = file('long.txt','a').write(ut+'\n')
+			xpath = "//div[@id='skip-to-navigation']/ul[@class='breadCrumbs']/li[@class='nonSearchPageChevron']/a" 
+			click(browser,xpath)
+			flag+=1
+	
+	browser.quit()
+
+def my_thread(threads_num):
+	threads = [] 
+	for i in range(0,threads_num):
+		threads_name = 'thread_'+str(i+1)
+		t = threading.Thread(target=cit_current)
+		threads.append(t)
+   	for i in range(0,len(threads)):
+   		threads[i].start() 
+   		print 'start thread '+str(i+1)
+   		time.sleep(10)
+   	for thread in threads:
+   		thread.join()
+
+if __name__=='__main__':
+	filename = '2012_papers.txt'	
+	paper_info(filename)
+	f = open('cur_year_cit.txt','a').write('UT\tPY\twos_Cit\n')
+	start_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+	print 'start time',start_time
+	my_thread(5)
+	end_time = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+	print 'end time',end_time
+
